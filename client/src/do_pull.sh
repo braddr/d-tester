8b2927222fa4f13e1008469ca7b15a601988a62a
Martin Nowak <code@dawg.eu>
2013-07-24 19:16:18 +0200

    use FETCH_HEAD instead of named remote

--- client/src/do_pull.sh
+++ client/src/do_pull.sh
@@ -23,18 +23,11 @@ echo -e "\tmerging pull: $3 $4 $5"
 
 cd $top/$1/$3
 
-echo "setting up remote topull -> $4" >> $top/$1/$3-merge.log 2>&1
-git remote add topull $4 >> $top/$1/$3-merge.log 2>&1
-if [ $? -ne 0 ]; then
-    echo -e "\tfailed to setup pull repo"
-    exit 1
-fi
-
 echo >> $top/$1/$3-merge.log
 N=3;
 for i in `seq 1 $N`; do
-    echo "fetching contents of $4 (attempt: $i/$N)" >> $top/$1/$3-merge.log 2>&1
-    git fetch topull         >> $top/$1/$3-merge.log 2>&1
+    echo "fetching $4 $5 (attempt: $i/$N)" >> $top/$1/$3-merge.log 2>&1
+    git fetch $4 $5 >> $top/$1/$3-merge.log 2>&1
     if [ $? -eq 0 ]; then
         break
     fi
@@ -46,8 +39,8 @@ for i in `seq 1 $N`; do
 done
 
 echo >> $top/$1/$3-merge.log
-echo "merging topull/$5" >> $top/$1/$3-merge.log
-git merge topull/$5      >> $top/$1/$3-merge.log 2>&1
+echo "merging FETCH_HEAD" >> $top/$1/$3-merge.log
+git merge FETCH_HEAD >> $top/$1/$3-merge.log 2>&1
 if [ $? -ne 0 ]; then
     echo -e "\tfailed to merge pull repo"
     exit 1

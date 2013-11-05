37503c45cd39601d13d9b19f49a0637c21c93da8
Brad Roberts <braddr@puremagic.com>
2013-11-04 22:42:28 -0800

    add support for executing the github pull merge
    1) When toggling a pull auto merge state, if turning on and the project allows auto merging, check for complete passing tests
    2) On test run complete, check for auto merge on for both the pull and the project, check for complete passing tests
    
    For both, if all three checks pass, call the github pull merge api.

--- serverd/p_toggle_auto_merge.d
+++ serverd/p_toggle_auto_merge.d
@@ -1,5 +1,6 @@
 module p_toggle_auto_merge;
 
+static import p_finish_pull_run;
 import std.conv;
 import std.range;
 
@@ -30,6 +31,28 @@ bool updateStore(string ghp_id)
     return true;
 }
 
+bool checkMergeNow(string projectid, string repoid, string pullid, string ghp_id, Appender!string outstr)
+{
+    sqlrow[] rows;
+
+    sql_exec(text("select p.name, r.name, p.allow_auto_merge, ghp.auto_pull from projects p, repositories r, github_pulls ghp where p.id = ", projectid, " and r.id = ", repoid, " and ghp.id = ", ghp_id));
+    rows = sql_rows();
+    if (rows.length < 1)
+    {
+        outstr.put("checkMergeNow: should have gotten exactly one row back from db");
+        return false;
+    }
+
+    // get out unless both project and pull are request auto-merging
+    if (rows[0][2] != "1" || rows[0][3] != "1")
+        return true;
+
+    if (!p_finish_pull_run.mergeGithubPull(rows[0][0], rows[0][1], pullid, ghp_id, outstr))
+        return false;
+
+    return true;
+}
+
 void run(const ref string[string] hash, const ref string[string] userhash, Appender!string outstr)
 {
     string raddr = lookup(hash, "REMOTE_ADDR");
@@ -39,15 +62,16 @@ void run(const ref string[string] hash, const ref string[string] userhash, Appen
     string ghp_id = lookup(userhash, "ghp_id");
 
     auto valout = appender!string;
-    if (!validateInput(raddr, projectid, repoid, pullid, ghp_id, valout))
+    if (
+            !validateInput(raddr, projectid, repoid, pullid, ghp_id, valout) ||
+            !updateStore(ghp_id) ||
+            !checkMergeNow(projectid, repoid, pullid, ghp_id, valout)
+       )
     {
         outstr.put("Content-type: text/plain\n\n");
         outstr.put(valout.data);
         return;
     }
-
-    if (!updateStore(ghp_id))
-        return;
 
     outstr.put(text("Location: http://", lookup(hash, "SERVER_NAME"), "/test-results/pull-history.ghtml?",
                "projectid=", projectid, "&",

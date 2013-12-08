e735022f00471b99bd7e2d351d029dbfcca47e96
Brad Roberts <braddr@puremagic.com>
2013-12-08 11:49:06 -0800

    move build output to obj directory

--- serverd/run.sh
+++ serverd/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while [ 1 ]; do
-    SERVERD_CONFIG="/home/braddr/sandbox/d/d-tester/serverd/config.json" ./update-pulls-prod
+    SERVERD_CONFIG="/home/braddr/sandbox/d/d-tester/serverd/config.json" ./obj/update-pulls-prod
     sleep 10m
 done

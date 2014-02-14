19452ec89c6bcdf1cb53dfb31b1647ffed88db35
Brad Roberts <braddr@puremagic.com>
2014-02-14 11:32:54 -0800

    fix isPullRunAborted to use deleted, not rc

--- serverd/clientapi/sql.d
+++ serverd/clientapi/sql.d
@@ -5,7 +5,7 @@ import std.conv;
 
 bool isPullRunAborted(string runid)
 {
-    sql_exec(text("select rc from pull_test_runs where id = ", runid));
+    sql_exec(text("select deleted from pull_test_runs where id = ", runid));
     sqlrow[] rows = sql_rows();
 
     if (rows.length != 1) return true;

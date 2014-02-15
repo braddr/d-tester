d3e7a5093abfe413ee5c7cb29437c184e89b99dd
Brad Roberts <braddr@puremagic.com>
2014-02-15 15:48:26 -0800

    always record the completed test results, even if aborting the run

--- serverd/clientapi/finish_pull_test.d
+++ serverd/clientapi/finish_pull_test.d
@@ -71,12 +71,13 @@ void run(const ref string[string] hash, const ref string[string] userhash, Appen
         return;
 
     updateHostLastCheckin(hostid, clientver);
+
+    sql_exec(text("update pull_test_data set end_time=now(), rc=", rc, " where id=", testid));
+
     if (isPullRunAborted(runid))
     {
         writelog("  aboring in progress pull test, runid: %s", runid);
         outstr.put("abort");
     }
-    else
-        sql_exec(text("update pull_test_data set end_time=now(), rc=", rc, " where id=", testid));
 }
 

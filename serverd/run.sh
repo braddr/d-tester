28004fdff5ba477d98f4f011d88aac51d8145987
Brad Roberts <braddr@puremagic.com>
2014-04-26 22:50:11 +0000

    update run.sh due to file system layout changes

--- serverd/run.sh
+++ serverd/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while [ 1 ]; do
-    SERVERD_CONFIG="/home/braddr/sandbox/d/d-tester/serverd/config.json" ./obj/update-pulls-prod
+    SERVERD_CONFIG="/home/ec2-user/auto-tester.puremagic.com/config.json" ./obj/update-pulls-prod
     sleep 10m
 done

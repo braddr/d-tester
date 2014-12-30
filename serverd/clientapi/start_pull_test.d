ace5afe2f9d2dac9910af95bd9e99ab5dbc0132d
Brad Roberts <braddr@puremagic.com>
2014-12-30 21:37:42 +0000

    fix 2 stragglers to use project_repositories

--- serverd/clientapi/start_pull_test.d
+++ serverd/clientapi/start_pull_test.d
@@ -47,7 +47,7 @@ bool validate_repoid(string runid, string repoid, Appender!string outstr)
     if (!validate_id(repoid, "repoid", outstr))
         return false;
 
-    sql_exec(text("select r.id from repositories r where r.project_id = (select pr.project_id from repositories r, project_repositories pr, github_pulls ghp, pull_test_runs tr where tr.id = ", runid, " and tr.g_p_id = ghp.id and ghp.repo_id = r.id and r.id = pr.repository_id) and r.id = ", repoid));
+    sql_exec(text("select r.id from repositories r, project_repositories pr where pr.project_id = (select pr.project_id from repositories r, project_repositories pr, github_pulls ghp, pull_test_runs tr where tr.id = ", runid, " and tr.g_p_id = ghp.id and ghp.repo_id = r.id and r.id = pr.repository_id) and pr.repository_id = r.id and r.id = ", repoid));
 
     sqlrow[] rows = sql_rows();
 

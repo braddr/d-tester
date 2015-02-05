a037a40b361459cee0d1cc898094adedd4f96f8d
Brad Roberts <braddr@puremagic.com>
2015-02-05 00:44:46 +0000

    improve handling when there is no HOST_DC

--- client/src/do_build_dmd.sh
+++ client/src/do_build_dmd.sh
@@ -23,7 +23,7 @@ cd $1/dmd/src
 HOST_DC=`ls -1 $top/release-build/install/*/$BINDIR/dmd$EXE`
 echo "HOST_DC=$HOST_DC" >> ../../dmd-build.log 2>&1
 
-if [ "$2" == "Win_32" -o "$2" == "Win_64" ]; then
+if [[ ! -z "$HOST_DC" && ( "$2" == "Win_32" || "$2" == "Win_64" ) ]]; then
     HOST_DC=`cygpath -w $HOST_DC`
     echo "HOST_DC=$HOST_DC" >> ../../dmd-build.log 2>&1
 fi
